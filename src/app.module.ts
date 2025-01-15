74845e46df9246cff17ed1b031006840e8da5816
louamlemjid <elmejidlouem@gmail.com>
2025-01-15 09:35:36 +0100

    added admin, user and designer resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { DesignerModule } from './designer/designer.module';
+import { UserModule } from './user/user.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, DesignerModule, UserModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
